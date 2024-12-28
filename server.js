6785a2d5a3b5451fe6269d2149f9ec140bed3868
Ademola Aderemi <158827677+cridiv@users.noreply.github.com>
2024-12-28 05:48:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.listen(4000, () => {
     console.log("Server is running on port 4000");
 });
